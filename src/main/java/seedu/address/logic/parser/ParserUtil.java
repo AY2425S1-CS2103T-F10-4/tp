b02a91415393faf068db2ba4e1607805461b113c
rynlwhh <e1121363.u.nus.edu>
2024-10-02 14:04:10 +0800

    Add remark command

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -13,6 +13,7 @@ import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Phone;
+import seedu.address.model.person.Remark;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -120,5 +121,17 @@ public class ParserUtil {
             tagSet.add(parseTag(tagName));
         }
         return tagSet;
+    }
+    /**
+     * Parses a {@code String remark} into a {@code Remark}.
+     * @param remark The remark to parse.
+     * @return The parsed Remark object.
+     * @throws ParseException if the given {@code remark} is invalid.
+     */
+    public static Remark parseRemark(String remark) throws ParseException {
+        requireNonNull(remark);
+        String trimmedRemark = remark.trim();
+        // No validation needed for remark content (can be any string), but can add if desired
+        return new Remark(trimmedRemark);
     }
 }
